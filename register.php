d3cab7dc6853dcd6eb224e7a69e02e758342c126
MassimoSchiavo <massimo.schiavo@mail.polimi.it>
2017-05-19 00:41:28 +0200

    added token bot

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://oratorioprova.herokuapp.com/execute.php';
+$BOT_TOKEN = '323732341:AAHeG_XRJNmwxRTNvDy7RmJFxNJXo7-hPFk';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
